91b8818f551b3d07e06460c88c40bfcdfeeae995
Victor T <vtimiojo@gmail.com>
2021-04-06 17:39:30 +0100

    Added gitignore, Procfile, node engine dependency, and updated ports

--- app.js
+++ app.js
@@ -121,6 +121,12 @@ app.get("/:customListPage", function(req, res) {
 
 })
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port);
+
+app.listen(port, function() {
     console.log("Server is live");
 })
